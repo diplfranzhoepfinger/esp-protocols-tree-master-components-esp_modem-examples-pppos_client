b4d8fb767bd18c319b74ece2f7aa48e40a0321d0
Franz Höpfinger <mail@franzhoepfinger.de>
2023-10-25 14:40:05 +0200

    feat: Add modem power key functionality and integrate PPPoS client
    This commit adds the necessary code to initialize and control the modem power key. It also integrates the PPPoS client into the main application flow.

--- pppos_client/main/main.c
+++ pppos_client/main/main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
+#include "modem_pwkey.h"
+
+extern void pppos_client_main(void);
 
 void app_main(void)
 {
-    while (true) {
-        printf("Hello from app_main!\n");
-        sleep(1);
-    }
+
+
+
+    init_modem_pwkey();
+
+    power_down_modem_pwkey();
+
+    // power Up does work now. .
+    power_up_modem_pwkey();
+
+
+
+    pppos_client_main();
+
+
+
 }
